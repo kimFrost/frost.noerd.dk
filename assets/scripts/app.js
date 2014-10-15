5a906d2eb2017b8a59cae2e4ac8581ae7e79f348
Kim Frost <kim@noerd.dk>
2014-10-15 10:00:41 +0200

    (added) - init commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,17 @@
+(function (undefined) {
+    'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('NoerdDk', [])
+  .run(function () {
+    console.log('Main Application Run()');
+  });
+})();
